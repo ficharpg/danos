91d07b95654c0c01f75116b38937f0610d58b30a
unknown <stanvard0202@gmail.com>
2021-06-02 17:06:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var vidaAtual = 10
+var vidaMax = 10
+var vida = [vidaAtual,vidaMax]
+danos(vida)
+
+function danos(vida){
+var = html= ""
+	html += "<tr><td>Soco</td>"
+	html += "<td>?</td>"
+	html += "<td><button onClick='soco()'>ROLL</button></td></tr>"
+	
+	
+	 var tabeladanos = document.getElementById("tabeladanos")
+  tabeladanos.innerHTML = html
+}
